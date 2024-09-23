8711ff871dbfff200fdfa268ada823815f855b81
James Murry <jmurry2014@gmail.com>
2024-09-23 08:53:47 -0500

    completed navigation added a jquery filter that determines what page the user is on and alters the list item so essentially an :active effect in css

---
+++ assets/logic.js
@@ -0,0 +1,53 @@
+
+
+$(document).ready(function() {
+
+    function showSideBar() {
+        const sidebar = $('.sidebar');
+        $('.hamburger').click(function () {
+            sidebar.css("display", "flex");
+        });
+    }
+    
+    function hideSideBar() {
+        const sidebar = $('.sidebar');
+        $('.close').click(function () {
+            sidebar.css("display", "none");
+        });
+    }
+    
+    
+    // Get the current URL path
+    var currentPage = window.location.pathname;
+    $('.nav-list li a').filter(function() {
+        return this.href.includes(currentPage);
+    }).css('font-size', '20px' , 'font-weight', 'bold');  
+
+
+
+
+
+
+
+
+
+
+
+    showSideBar();
+    hideSideBar();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
