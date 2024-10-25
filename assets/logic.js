c26d0308ee16429dc7ba5617affc16490b861a3f
James Murry <jmurry2014@gmail.com>
2024-10-25 13:47:55 -0500

    finished out the website design jquery in place except for lab #5

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,49 @@
 
 $(document).ready(function() {
+ // Initialize Swiper with Coverflow effect
+ var swiper = new Swiper('.swiper', {
+    initialSlide: 1,
+    slidesPerView: 1,
+    freeMode: true,
+    loop: true,
+    spaceBetween: 32,
+    // effect: 'coverflow', // Add Coverflow effect
+    // coverflowEffect: {
+    //     rotate: 0, // Rotate the slides
+    //     stretch: 0, // Stretch the slides
+    //     depth: 100, // Depth of the slides
+    //     modifier: .3, // Multiplier effect
+    //     slideShadows: true, // Enable slide shadows
+    // },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            // Customize for screens >= 320px
+        },
+        480: {
+            // Customize for screens >= 480px
+        },
+        996: {
+            initialSlide: 1,
+            slidesPerView: 'auto',
+            spaceBetween: 32,
+            loop: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        }
+    }
+});
+
+
+
+
+
+
     $('body').css('display', 'none'); // Initially hide the body
     $('body').fadeIn();
 
